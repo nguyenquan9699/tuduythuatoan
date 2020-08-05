8d6e85914e63ff274301196f683eea2bbcd76422
1712694 <1712694@student.hcmus.edu.vn>
2020-08-05 18:13:13 +0700

    init

---
+++ Bai12/Bai12.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    fstream fin, fout;
+    fin.open("input", ios_base::in);fout.open("output", ios_base::out);
+
+    
+
+    fin.close(); fout.close();
+    return 0;
+}
