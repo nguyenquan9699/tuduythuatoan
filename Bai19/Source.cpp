41b1da41d7c51ca35f6b2a8d8355a7d53d6eb5a0
1712514 <1712514@student.hcmus.edu.vn>
2020-08-09 14:54:11 +0700

    Done
    ABCD

--- Bai19/Source.cpp
+++ Bai19/Source.cpp
@@ -1,8 +1,84 @@
 #include<iostream>
-
+#include<vector>
+#include<time.h>
+#include <fstream>
+#include<algorithm>
 using namespace std;
+vector<int> a;
+vector<long long> prefixSum;
+void generate(int n) {
+	ofstream output;
+	output.open("output.txt");
+	srand(time(NULL));
+	int flag;
+	for (int i = 0; i < n; ++i) {
+		flag = rand() % 2 == 0 ? 1 : -1;
+		output << rand() % 1000000000 * flag << " ";
+	}
+	output.close();
+}
+void Input(int n) {
+	ifstream input;
+	input.open("output.txt");
+	int x;
+	for (int i = 0; i < n; i++) {
+		input >> x;
+		a.push_back(x);
+	}
+	input.close();
+}
+
+int sum(int i, int j) {
+	return prefixSum[j + 1] - prefixSum[i];
+}
+
+int Search(int start, int end, int x) {
+	if (sum(start, start) == x) return start;
+
+	int l = start, r = end;
+
+	while (r - l > 1) {
+		int mid = (l + r) / 2;
+		int s = sum(start, mid);
+		if (s >= x)
+		{
+			r = mid;
+		}
+		else
+			l = mid;
+	}
+	if (sum(start, r) == x) return r;
+	return -1;
+}
 
 int main() {
+	int n;
+	cin >> n;
+	//generate(n);
+	Input(n);
+	int x;
+	cin >> x;
 
+	// init prefixSum
+	prefixSum.push_back(0);
+	for (int i = 0; i < 2 * n; i++)
+		prefixSum.push_back(prefixSum[i] + a[i % n]);
+
+	int s = 0, e = n + 1;
+
+	for (int i = 0; i < n; i++)
+	{
+		int p = Search(i, i + n, x);
+		if (p != -1)
+		{
+			if (p - i < e - s)
+			{
+				s = i;
+				e = p;
+			}
+		}
+
+	}
+	cout << s << " " << e % n << endl;
 	return 0;
 }
--- Bai23/Source.cpp
+++ Bai23/Source.cpp
@@ -1,8 +1,101 @@
 #include<iostream>
-
+#include<iostream>
+#include<vector>
+#include<time.h>
+#include <fstream>
+#include<algorithm>
 using namespace std;
-
+class Point {
+public:
+	int x, y;
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	int DienTichDaiSo(Point a, Point b) {
+		return x * (b.y - a.y) + a.x * (y - b.y) + b.x * (a.y - y);
+	}
+	friend ostream& operator<<(ostream& os, Point& a) {
+		os << a.x << " " << a.y << endl;
+		return os;
+	}
+};
+vector<Point> _n;
+vector<Point> _m;
+vector<int> up;
+vector<int> low;
+void Input() {
+	int m, n;
+	ifstream input;
+	input.open("output.txt");
+	int x;
+	int y;
+	input >> n;
+	for (int i = 0; i < n; i++) {
+		input >> x >> y;
+		_n.push_back(Point(x,y));
+	}
+	input >> m;
+	for (int i = 0; i < m; i++) {
+		input >> x >> y;
+		_m.push_back(Point(x, y));
+	}
+	input.close();
+}
+bool Check(Point X, int imax, int imin) {
+	if (X.x >= _n[imax].x || X.x <= _n[imin].x) {
+		return false;
+	}
+	// xet X nam tren hay duoi
+	if (_n[imin].DienTichDaiSo(_n[imax], X) < 0) {
+		// X nam tren
+		int l = 0, r = up.size() - 1;
+		while (r - l > 1) {
+			int mid = (r + l) / 2;
+			if (X.x > _n[up[mid]].x) l = mid;
+			else r = mid;
+		}
+		return _n[up[l]].DienTichDaiSo(_n[up[r]], X) <= 0 ? false : true;
+	}
+	else if (_n[imin].DienTichDaiSo(_n[imax], X) > 0) {
+		// X nam duoi
+		int l = 0, r = low.size() - 1;
+		while (r - l > 1) {
+			int mid = (r + l) / 2;
+			if (X.x > _n[low[mid]].x) l = mid;
+			else r = mid;
+		}
+		return _n[low[l]].DienTichDaiSo(_n[low[r]], X) >= 0 ? false : true;
+	}
+	// X nam tren imax imin thi thuoc da giac loi
+	return true;
+}
 int main() {
+	Input();
 
+	// tm xmin, xmax
+	int ixmax = 0, ixmin = 0;
+	for (int i = 1; i < _n.size(); i++) {
+		if (_n[ixmin].x > _n[i].x) ixmin = i;
+		else if (_n[ixmax].x < _n[i].x) ixmax = i;
+	}
+
+	// xay dung danh sach cac diem nua tren
+	for (int i = ixmin; i != ixmax; i = (i-1+_n.size())%_n.size()){
+		up.push_back(i);
+	}
+	up.push_back(ixmax);
+	// xay dung danh sach cac diem nua duoi
+	for (int i = ixmin; i != ixmax; i = (i + 1 + _n.size()) % _n.size()) {
+		low.push_back(i);
+	}
+	low.push_back(ixmax);
+	vector<Point> res;
+	for (int i = 0; i < _m.size(); i++) {
+		if (Check(_m[i], ixmax, ixmin)) res.push_back(_m[i]);
+	}
+
+	for (int i = 0; i < res.size(); i++)
+		cout << res[i];
 	return 0;
 }
