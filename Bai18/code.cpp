e971244614def02c6cba5d6eab211935053a012b
1712514 <1712514@student.hcmus.edu.vn>
2020-08-11 06:16:36 +0700

    Huy
    HUy

--- Bai18/code.cpp
+++ Bai18/code.cpp
@@ -7,27 +7,34 @@ using namespace std;
 
 vector<int> Bai18(vector<int>a, int n, int k) {
 	vector<int> s;
-	s.push_back(a[0]);
-	for (int i = 1; i < n; i++) {
-		s.push_back(s[i - 1] + a[i]);
+	s.push_back(0);
+	for (int i = 0; i < n; i++) {
+		s.push_back(s[i] + a[i]);
 	}
 
 	int id_l = 0;
 	int id_r = k - 1;
-	int max = s[id_r];
-	int min = s[id_l];
+	int max = s[k];
+	int min = 0;
 	int tmp = id_l;
-	
-	for (int i = 0; i < n; i++) {
-		if (s[i] < min) {
-			min = s[i];
-			tmp = i;
+
+	for (int i = 1; i <= n; i++) {
+
+		if (s[i] > max && i >= k) {
+			max = s[i];
+			id_r = i - 1;
+			id_l = 0;
 		}
 
-		if (id_r - tmp  >= k && s[i] - min > max) {
+		if (s[i] < min) {
+			min = s[i];
+			tmp = i - 1;
+		}
+
+		if (i - tmp >= k && s[i] - min > max) {
 			max = s[i] - min;
-			id_r = i;
-			id_l = tmp + 1;
+			id_r = i - 1;
+			id_l = tmp;
 		}
 	}
 	vector<int> res;
@@ -41,14 +48,12 @@ vector<int> Bai18(vector<int>a, int n, int k) {
 int main() {
 	int n;
 	vector<int> a;
-	int b[9] = { 3,3,0,8,4,-2,-8,-4,13};
+	int b[9] = { 3,-3,5,-7,1,-7};
 	for (int i = 0; i < 6; i++) {
 		a.push_back(b[i]);
 	}
 
-	int res = Bai18(a, 9, 4);
-	cout << res;
-
-	system("pause");
+	vector<int> res = Bai18(a, 6, 4);
+	cout << res[0] << res[1];
 
 }
